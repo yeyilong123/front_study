bfe8dba4aff1bcb39ce9cb01c64331389ea2e198
yeyilong123 <1240182124@qq.com>
2019-04-09 17:10:33 +0800

    webpack

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlPlugin = require('html-webpack-plugin');
 module.exports = {
     mode: 'development',
     entry: {
@@ -7,6 +8,29 @@ module.exports = {
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: '[name].js'
+    },
+    module:{
+        rules:[
+            {
+                test: /\.css$/,
+                use: ['style-loader','css-loader']
+            }
+        ]
+    },
+    plugins:[
+        new HtmlPlugin({
+            template: './src/index.html',
+            minify:{
+                removeAttributeQuotes: true
+            },
+            hash: true,
+
+        })
+    ],
+    devServer:{
+        contentBase: path.resolve(__dirname,'dist'),
+        host: 'localhost',
+        port: 8081
     }
 
 }
