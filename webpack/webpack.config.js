79a8555c94d410675700bf7e96846caf89128a8a
yeyilong123 <1240182124@qq.com>
2019-04-10 14:38:31 +0800

    webpack_css

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
 module.exports = {
     mode: 'development',
     entry: {
@@ -13,7 +14,21 @@ module.exports = {
         rules:[
             {
                 test: /\.css$/,
-                use: ['style-loader','css-loader']
+                // use: ['style-loader','css-loader']
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: "css-loader"
+                })
+            },
+            {
+                test: /\.(jpg|png|gif)$/,
+                use: [{
+                    loader: "url-loader",
+                    options:{
+                        limit: 500,
+                        outputPath: "imgs/"
+                    }
+                }]
             }
         ]
     },
@@ -24,8 +39,8 @@ module.exports = {
                 removeAttributeQuotes: true
             },
             hash: true,
-
-        })
+        }),
+        new ExtractTextPlugin("index.css")
     ],
     devServer:{
         contentBase: path.resolve(__dirname,'dist'),
