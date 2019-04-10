90bcd0d3f49a15d7fbbaa1eb67c2b61d16927834
yeyilong123 <1240182124@qq.com>
2019-04-10 17:39:07 +0800

    注释

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const HtmlPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const glob = require('glob');
+const PurifyCSSPlugin = require("purifycss-webpack");
+const webpack = require("webpack");
 module.exports = {
     mode: 'development',
     entry: {
@@ -14,10 +17,12 @@ module.exports = {
         rules:[
             {
                 test: /\.css$/,
-                // use: ['style-loader','css-loader']
                 use: ExtractTextPlugin.extract({
                     fallback: "style-loader",
-                    use: "css-loader"
+                    use: [{
+                        loader: "css-loader",
+                        options: {importLoaders: 1}
+                    }, "postcss-loader"]
                 })
             },
             {
@@ -29,7 +34,30 @@ module.exports = {
                         outputPath: "imgs/"
                     }
                 }]
+            },
+            {
+                test: /\.(htm|html)$/i,
+                loader: 'html-withimg-loader'
+            },
+            {
+                test: /\.scss$/,
+                // use: ['style-loader','css-loader','sass-loader']
+                use: ExtractTextPlugin.extract({
+                    use: ['css-loader','sass-loader'],
+                    fallback: 'style-loader',
+                })
+            },
+            {
+                test: /\.js$/,
+                use: [{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }],
+                exclude: /node_modules/
             }
+            
         ]
     },
     plugins:[
@@ -40,12 +68,17 @@ module.exports = {
             },
             hash: true,
         }),
-        new ExtractTextPlugin("index.css")
+        new ExtractTextPlugin("index.css"),
+        new PurifyCSSPlugin({
+            paths: glob.sync(path.join(__dirname, 'src/*.html')),
+        }),
+        new webpack.BannerPlugin('翻版必究')
     ],
     devServer:{
         contentBase: path.resolve(__dirname,'dist'),
         host: 'localhost',
         port: 8081
-    }
+    },
+    
 
 }
