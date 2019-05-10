5bf6acb61c90c8833035c67427e3c93c5bbcc6e6
yeyilong123 <1240182124@qq.com>
2019-05-10 20:17:27 +0800

    vue

---
+++ vue_app/src/components/CommonHeader.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <div class="header">
+      <span>首页</span>
+      <span>电影</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+    .header{
+        height: 1rem;
+        background: #f00;
+    }
+</style>
